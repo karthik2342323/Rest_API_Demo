c5fa27bcfb5913c20b72a3ecb528a94218ff2938
karthik2342323 <51212828+karthik2342323@users.noreply.github.com>
2021-05-08 20:35:59 +0530

    Update userRoutes.js

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -4,6 +4,7 @@ const userComponents = require('../components/user');
 const myInfo=require('../components/getInfo');
 const users=require('../components/FollowUser');
 const forExp=require('../components/forEXP_DEBUGG');
+const  data=require('../components/forEXP_DEBUGG');
 
 router.post('/signup',userComponents.signup);
 
@@ -12,10 +13,18 @@ router.post('/login',userComponents.login);
 router.post('/getMyData',myInfo.getInfo);
 router.post('/listOfUser',users.List);
 router.post('/toFollow',users.follow);
-router.get('/forExp',forExp.first123456);
 
 router.delete('/delete/:userID/:token',userComponents.deleteUser);
 
 router.post('/updateAccount',userComponents.updateAccount);
 
+
+
+router.get('/forExp',forExp.first123456);
+router.post('/putData',data.putJSONData);
+router.post('/addData',data.ADDJSONData);
+router.post('/removeFollowing',users.removeFollowing);
+router.post('/forExpDataFlow',forExp.testFetchingDatas);
+
 module.exports = router;
+
